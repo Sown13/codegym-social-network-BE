cfb06610b54590abf42e9becdbbd91eba17e319f
Sown13 <sonnh.buh@gmail.com>
2023-07-19 10:38:48 +0700

    first frame init

---
+++ src/main/java/com/example/social_network/model/User.java
@@ -0,0 +1,8 @@
+package com.example.social_network.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+public class User {
+}
---
+++ src/main/java/com/example/social_network/service/user/IUserService.java
@@ -0,0 +1,7 @@
+package com.example.social_network.service.user;
+
+import com.example.social_network.model.User;
+import com.example.social_network.service.IGeneralService;
+
+public interface IUserService extends IGeneralService<User> {
+}
