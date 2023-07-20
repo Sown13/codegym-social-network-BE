d97e90706aa2f231c4473c3e3bda996332c332b6
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-07-20 09:08:16 +0700

    chua lam gi

--- src/main/java/com/example/social_network/controller/UserRestController.java
+++ src/main/java/com/example/social_network/controller/UserRestController.java
@@ -1,47 +1,4 @@
 package com.example.social_network.controller;
 
-import com.example.social_network.model.User;
-import com.example.social_network.service.user.IUserService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
-
-@RestController
-@CrossOrigin("*")
-@RequestMapping("/users")
 public class UserRestController {
-    @Autowired
-    private IUserService userService;
-    @GetMapping()
-    private ResponseEntity<Iterable<User>>findAllUsers(){
-        Iterable<User>listUser=userService.findAll();
-        return new ResponseEntity<>(listUser, HttpStatus.OK);
-    }
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
-        if (result.hasErrors()) {
-            return ResponseEntity.badRequest().body(result.getAllErrors());
-        }
-        try {
-            userService.save(user);
-            return ResponseEntity.ok("User registered successfully");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-    @GetMapping("/{id}")
-    private ResponseEntity<Optional<User>>findUserByid(@PathVariable ("id")Long id){
-        Optional<User>user=userService.findById(id);
-        if(user.isPresent()){
-            return new ResponseEntity<>(user,HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
 }
