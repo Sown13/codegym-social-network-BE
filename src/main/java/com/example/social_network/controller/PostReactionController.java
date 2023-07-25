2e633ab49891ea4e3cb205ab9ca102b10d83f66b
Sown13 <sonnh.buh@gmail.com>
2023-07-26 00:34:34 +0700

    done counting number of post-reaction

--- src/main/java/com/example/social_network/controller/PostReactionController.java
+++ src/main/java/com/example/social_network/controller/PostReactionController.java
@@ -1,9 +1,7 @@
 package com.example.social_network.controller;
 import com.example.social_network.service.post.post_reaction.IPostReactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -11,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostReactionController {
     @Autowired
     private IPostReactionService postReactionService;
+
+    @GetMapping("/post/{id}/total-like")
+    public Long countLikeOfPost(@PathVariable Long id){
+        return postReactionService.countPostReactionByPostId(id);
+    }
 }
