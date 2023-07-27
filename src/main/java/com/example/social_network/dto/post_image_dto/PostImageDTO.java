cc5c84f5ccd70ad19ed6670ace4a6ef7ea881807
Sown13 <sonnh.buh@gmail.com>
2023-07-27 12:31:16 +0700

    add controller add a list of post image

--- src/main/java/com/example/social_network/dto/post_image_dto/PostImageDTO.java
+++ src/main/java/com/example/social_network/dto/post_image_dto/PostImageDTO.java
@@ -1,10 +1,23 @@
 package com.example.social_network.dto.post_image_dto;
 
+import com.example.social_network.model.post.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class PostImageDTO {
+    private Long postImageId;
+    private String imgUrl;
+    private Date dateCreated;
+    private Post post;
 }
